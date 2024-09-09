bcbd44961feda45c24206721d182bb6f5e7158bc
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-09-09 14:25:24 +0545

    passwordGenerator

--- script.js
+++ script.js
@@ -13,87 +13,87 @@ const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+=";
 
 function getLowercase() {
-    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
 }
 
 function getUppercase() {
-    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
 }
 
 function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)];
+  return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
 function getSymbol() {
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 function generatePassword() {
-    const len = lenEl.value;
+  const len = lenEl.value > 30 ? 30 : lenEl.value < 2 ? 2 : lenEl.value;
 
-    let password = "";
+  let password = "";
 
-    if (upperEl.checked) {
-        password += getUppercase();
-    }
+  if (upperEl.checked) {
+    password += getUppercase();
+  }
 
-    if (lowerEl.checked) {
-        password += getLowercase();
-    }
+  if (lowerEl.checked) {
+    password += getLowercase();
+  }
 
-    if (numberEl.checked) {
-        password += getNumber();
-    }
+  if (numberEl.checked) {
+    password += getNumber();
+  }
 
-    if (symbolEl.checked) {
-        password += getSymbol();
-    }
+  if (symbolEl.checked) {
+    password += getSymbol();
+  }
 
-    for (let i = password.length; i < len; i++) {
-        const x = generateX();
-        password += x;
-    }
+  for (let i = password.length; i < len; i++) {
+    const x = generateX();
+    password += x;
+  }
 
-    pwEl.innerText = password;
+  pwEl.innerText = password;
 }
 
 function generateX() {
-    const xs = [];
-    if (upperEl.checked) {
-        xs.push(getUppercase());
-    }
+  const xs = [];
+  if (upperEl.checked) {
+    xs.push(getUppercase());
+  }
 
-    if (lowerEl.checked) {
-        xs.push(getLowercase());
-    }
+  if (lowerEl.checked) {
+    xs.push(getLowercase());
+  }
 
-    if (numberEl.checked) {
-        xs.push(getNumber());
-    }
+  if (numberEl.checked) {
+    xs.push(getNumber());
+  }
 
-    if (symbolEl.checked) {
-        xs.push(getSymbol());
-    }
+  if (symbolEl.checked) {
+    xs.push(getSymbol());
+  }
 
-    if (xs.length === 0) return "";
+  if (xs.length === 0) return "";
 
-    return xs[Math.floor(Math.random() * xs.length)];
+  return xs[Math.floor(Math.random() * xs.length)];
 }
 
 generateEl.addEventListener("click", generatePassword);
 
 copyEl.addEventListener("click", () => {
-    const textarea = document.createElement("textarea");
-    const password = pwEl.innerText;
+  const textarea = document.createElement("textarea");
+  const password = pwEl.innerText;
 
-    if (!password) {
-        return;
-    }
+  if (!password) {
+    return;
+  }
 
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand("copy");
-    textarea.remove();
-    alert("Password copied to clipboard");
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
 });
